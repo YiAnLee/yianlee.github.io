2fa8558725ed2ab5e2215a0c973cf8447a01010d
YiAnLee <andy75527552andy@gmail.com>
2017-06-08 00:22:16 +0800

    initialize
    initialize

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    // $('label').click(function(event){
+    //     $('p').fadeToggle(1000);
+    // });
+    // $('label').click(function(event){
+    //     console.log("k")
+    //     $('.image_list').append( "<div class='imgbox thumbnail'><img id='output1' class='input_image'></div>")
+    // });
+})
